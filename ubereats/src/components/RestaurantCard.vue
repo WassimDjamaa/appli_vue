a7802be0e910a9e942a9cecf249deccd7aef9fce
Wassim DJAMAA <56625264+WassimDjamaa@users.noreply.github.com>
2023-04-23 19:19:48 +0200

    create my first component restaurantCard

--- ubereats/src/components/RestaurantCard.vue
+++ ubereats/src/components/RestaurantCard.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="restaurant--card">
-    <p>Mon premier composant</p>
+    <div class="restaurant--image">
+
+    </div>
+
+    <div class="restaurant--information">
+        <div class="top">
+            <p class="name">
+                Subway
+            </p>
+
+            <p class="note">
+                <span>
+                    4.5
+                </span>
+            </p>
+        </div>
+
+        <p class="time">
+            20-30mn
+        </p>
+    </div>
+
   </div>
 </template>
 
@@ -11,11 +32,40 @@ export default {
 
 <style lang="scss">
 .restaurant--card {
-    width: 32%;
-    height: 30vh;
-
+  width: 32%;
+  height: 30vh;
   p{
     margin: 0px;
   }
-}
-</style>
+
+  .restaurant--image {
+    background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==");
+    height: 70%;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .restaurant--information {
+    .top {
+      margin: 10px 0px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .note {
+        background-color: aliceblue;
+        height: 30px;
+        width: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.8rem;
+        border-radius: 50%;
+      }
+    }
+
+    .time {
+      font-size: 0.9rem;
+    }
+  }
+}</style>
